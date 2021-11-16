69c538d8cfc0339353f35f0a226c327941e82fe3
Dan <Danielwhite98@icloud.com>
2021-11-16 21:14:20 +0000

    homepg

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.css';
+// import './img/icons/css/ionicons.css';
+// import './img/font-awesome/css/font-awesome.css';
+// import 'jquery/dist/jquery.min.js';
+
+
+import './style.css';
+
+
+import Navbar from './components/navbar.jsx';
+import Intro from './components/intro.jsx';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <Navbar />
+      <Intro />
+  </React.StrictMode>,
+document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
