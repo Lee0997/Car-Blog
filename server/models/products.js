01adde937c70640da6f9863912d8d0ff58428cba
Abdifatah <abdifatah.101@outlook.com>
2021-11-20 10:54:05 +0000

    Sorted models

--- server/models/products.js
+++ server/models/products.js
@@ -1,7 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model} = require('mongoose');
 const bcrypt = require('bcrypt');
 const { Products } = require('.');
 
+class Products extends Model {
+}
 
+Products.innit({
+    id: {
+        type: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+
+    },
+
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+
+    },
+
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    make: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    range: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+
+    year: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+    
+})
+    
 
 module.exports = Products
