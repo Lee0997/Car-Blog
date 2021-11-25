f6e9f7fa4c6e403359d8b29cfcbd1f587ff267f8
Dan <Danielwhite98@icloud.com>
2021-11-25 02:33:45 +0000

    layout

--- src/components/regForm.jsx
+++ src/components/regForm.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 const RegistrationForm = () => (
+  <div id="home" className="intro route bg-image background">
+  <div id="stars" />
+  <div id="stars2" />
+  <div id="stars3" />
   <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
     <Grid.Column style={{ maxWidth: 450 }}>
       <Header as='h2' color='blue' textAlign='center'>
@@ -9,6 +13,14 @@ const RegistrationForm = () => (
       </Header>
       <Form size='large'>
         <Segment stacked>
+        <label>UserName:</label>
+          <Form.Input
+            fluid
+            icon='user'
+            iconPosition='left'
+            placeholder='User-Name'
+            type='User-name'
+          />
           <label>Please enter an email address below:</label>
           <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
           <label>Password:</label>
@@ -21,12 +33,13 @@ const RegistrationForm = () => (
           />
 
           <Button color='blue' fluid size='large'>
-            Sign Up
+            Sign Up 
           </Button>
         </Segment>
       </Form>
     </Grid.Column>
   </Grid>
+  </div>
 )
 
 export default RegistrationForm
