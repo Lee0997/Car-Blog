33d87518f9eecd69629afc6bf876bc925d05936a
Dan <Danielwhite98@icloud.com>
2021-12-03 10:38:14 +0000

    working locally

--- client/src/index.js
+++ client/src/index.js
@@ -1,56 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-
-} from "@apollo/client";
-
-const httpLink = createHttpLink({
-  uri: "http://localhost:3001"
-});
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-  const httpLink = createHttpLink({
-    uri: '/graphql',
-  });
-  
-  // Construct request middleware that will attach the JWT token to every request as an `authorization` header
-  const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    const token = localStorage.getItem('id_token');
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    };
-  });
-  
-  const client = new ApolloClient({
-    // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-
-
-});
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
+  <React.StrictMode>
     <App />
-  </ApolloProvider>,
-  document.getElementById('root'),
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-
